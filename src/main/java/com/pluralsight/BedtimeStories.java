79e56edb22298be5f47b0f9a6a9076a9153a9a9b
Erik Holden <erikholden4@gmail.com>
2023-10-16 15:16:22 -0400

    BedtimeStories

---
+++ src/main/java/com/pluralsight/BedtimeStories.java
@@ -0,0 +1,74 @@
+package com.pluralsight;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class BedtimeStories {
+    public static void main(String[] args) {
+        printStories();
+    }
+    public static void printStories() {
+        try {
+            Scanner kbScanner = new Scanner(System.in);
+            String storyOut;
+            System.out.print("Please enter a story title to read: \t\n- goldilocks\t\n- hansel_and_gretel\t\n- mary_had_a_little_lamb\nUser Input: ");
+            String fileChoice = kbScanner.nextLine();
+            if(fileChoice.equalsIgnoreCase("goldilocks")){
+                FileInputStream fis = new FileInputStream("src/main/resources/goldilocks.txt");
+                Scanner txtReader = new Scanner(fis);
+                while(txtReader.hasNextLine()){
+                    storyOut = txtReader.nextLine();
+                    System.out.println(storyOut);
+                }
+                txtReader.close();
+                anotherStory();
+            }else if(fileChoice.equalsIgnoreCase("hansel_and_gretel")){
+                FileInputStream fis = new FileInputStream("src/main/resources/hansel_and_gretel.txt");
+                Scanner txtReader = new Scanner(fis);
+                while(txtReader.hasNextLine()){
+                    storyOut = txtReader.nextLine();
+                    System.out.println(storyOut);
+                }
+                txtReader.close();
+                anotherStory();
+            }else if(fileChoice.equalsIgnoreCase("mary_had_a_little_lamb")){
+                FileInputStream fis = new FileInputStream("src/main/resources/mary_had_a_little_lamb.txt");
+                Scanner txtReader = new Scanner(fis);
+                while(txtReader.hasNextLine()){
+                    storyOut = txtReader.nextLine();
+                    System.out.println(storyOut);
+                }
+                txtReader.close();
+                anotherStory();
+            }
+            else{
+                System.out.println("Please enter a valid file/story name. ");
+                printStories();
+            }
+        }
+        catch(IOException e){
+            System.out.println("Please enter a valid file/story name. ");
+            printStories();
+        }
+    }
+
+    public static void anotherStory() {
+        Scanner kbScanner = new Scanner(System.in);
+        System.out.print("Would you like to read another story? (1 or 2)\n\t1) Yes\n\t2) No\nUser Input: ");
+        int choice = kbScanner.nextInt();
+        switch(choice){
+            case 1:
+                printStories();
+            case 2:
+                System.out.println("Thank you for reading!");
+                break;
+            default:
+                System.out.println("Please enter a valid choice (1 or 2).");
+                anotherStory();
+        }
+        kbScanner.close();
+    }
+
+}
